91a62a2ecbfe277a7bb95c54a2582c24110261e8
gilles <gilles.faucherand@gmail.com>
2017-11-13 12:07:28 +0100

    step 4

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import PropTypes from 'prop-types';
 // const App = () => <h1>Hi 1</h1>
 
 class App extends React.Component {
+
+
+    constructor() {
+        super();
+        this.state = {
+            txt4: "this is state txt4",
+            txt5: "this is state txt5",
+        }
+
+    }
+
+    update(e) {
+        this.setState({txt4: e.target.value})
+    }
+
     render() {
         let txt2 = "prop value txt2"
         return (
@@ -17,6 +32,11 @@ class App extends React.Component {
             txt3 prop='{this.props.txt3}' 
             <br/>
             cat prop='{this.props.cat}'
+            <br/>
+            <input type="text" onChange={this.update.bind(this)}/>
+            txt4 prop='{this.state.txt4}'
+            <br/>
+            txt5 prop='{this.state.txt5}'
             <br/>
             <b>Bold</b>
             </div>
