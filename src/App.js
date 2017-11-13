11b36db52107045331a1be733151428aad2ed9d6
gilles <gilles.faucherand@gmail.com>
2017-11-13 11:46:35 +0100

    step 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
-const App = () => <h1>Hi</h1>
+// const App = () => <h1>Hi 1</h1>
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1>Hi 2</h1>
+            <b>Bold</b>
+            </div>
+            )
+    }
+}
 
 export default App
